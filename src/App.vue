92d9fd7f1df3ba1cae323b7001d2e2622715524e
mullerow <maximilian.kohler@posteo.de>
2024-04-08 21:50:46 +0200

    chore: add some dummy data

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,34 @@
 <script>
 //import { RouterView } from 'vue-router'
 
-export default {}
+export default {
+  data() {
+    return {
+      poiList: [
+        {
+          id: 1,
+          categorie: 'elevator',
+          'detail-categories': { 'elevator-size': 'large' },
+          xCoordinates: 52.55347266835718,
+          yCoordinates: 13.412074165422549,
+          minWidth: 0.92
+        },
+        {
+          id: 2,
+          categorie: 'toilet',
+          'detail-categories': {
+            'handicapped-accessible': true,
+            'changing station': true,
+            'free-of-charge': false
+          },
+          xCoordinates: 52.55347266835718,
+          yCoordinates: 13.412074165422549,
+          minWidth: 1.22
+        }
+      ]
+    }
+  }
+}
 </script>
 
 <template>
