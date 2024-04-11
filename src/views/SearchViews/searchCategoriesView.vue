5b75e3becdbe3c5437eaee697c99ce7627b9b2e2
Eric <rumperic@gmail.com>
2024-04-11 16:36:48 +0200

    feat: kategorie button speichert button text in local storage

--- src/views/SearchViews/searchCategoriesView.vue
+++ src/views/SearchViews/searchCategoriesView.vue
@@ -2,9 +2,23 @@
   <RouterLink :to="{ name: 'home' }">Gehe zurück zur Startseite</RouterLink>
   <RouterLink :to="{ name: 'searchpoi' }">Gehe eine Seite zurück</RouterLink>
   <h2>Search Kategorien</h2>
+  <CategorieButton id="kategorie" @click="saveButtonValue" />
+  <NavButton />
   <RouterLink :to="{ name: 'searchoptionalcategorie' }"> Weiter</RouterLink>
 </template>
 
 <script>
-export default {}
+import CategorieButton from '@/components/CategorieButton.vue'
+import NavButton from '@/components/NavButton.vue'
+
+export default {
+  components: { CategorieButton, NavButton },
+
+  methods: {
+    saveButtonValue() {
+      const buttonValue = document.getElementById('kategorie').textContent
+      localStorage.setItem('buttonValue', buttonValue)
+    }
+  }
+}
 </script>
