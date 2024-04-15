6fa4a3ad64c57fcafca1116e80511ec866863db4
Vanessa Pfohmann <v.pfohmann@googlemail.com>
2024-04-15 16:52:30 +0200

    dummytest

--- src/views/AccountViews/accountView.vue
+++ src/views/AccountViews/accountView.vue
@@ -4,27 +4,42 @@
   <h2>Verwalte deine Account-Daten</h2>
   <InputField
     label="Username"
-    v-model="userData.userName"
-    placeholder="username"
-    :disabled="!editMode"
+    v-model="store.userData[0].userName"
+    :placeholder="store.userData[0].userName ? store.userData[0].userName : 'username'"
   ></InputField>
-  <InputField label="Email" v-model="userData.eMailAddress" placeholder="email"></InputField>
-  <InputField label="password" v-model="userData.passWord" placeholder="passwort"></InputField>
+  <InputField
+    label="Email"
+    v-model="store.userData[0].eMailAddress"
+    :placeholder="store.userData[0].eMailAddress ? store.userData[0].eMailAddress : 'Email'"
+  ></InputField>
+  <InputField
+    label="password"
+    v-model="store.userData[0].passWord"
+    :placeholder="store.userData[0].passWord ? store.userData[0].passWord : 'Passwort'"
+  ></InputField>
   <InputField
     label="Mobility Assistance"
-    v-model="userData.mobilityAssistance"
-    placeholder="mobility assistance"
+    v-model="store.userData[0].mobilityAssistance"
+    :placeholder="
+      store.userData[0].mobilityAssistance
+        ? store.userData[0].mobilityAssistance
+        : 'mobility assistance'
+    "
   ></InputField>
   <InputField
     label="Mobility Assistance Width"
-    v-model="userData.mobilityAssistanceWidth"
-    placeholder="mobilityAssistance width"
+    v-model="store.userData[0].mobilityAssistanceWidth"
+    :placeholder="
+      store.userData[0].mobilityAssistanceWidth
+        ? store.userData[0].mobilityAssistanceWidth
+        : 'mobilityAssistance width'
+    "
   ></InputField>
-  <NavButton :Navigation="'Speichern'" @click="editUserData"></NavButton>
+  <NavButton :Navigation="'Speichern'" @click="saveUserData"></NavButton>
 </template>
 
 <script>
-//import { storeData } from '@/stores/store.js'
+import { storeData } from '@/stores/store.js'
 
 import InputField from '@/components/InputField.vue'
 import NavButton from '@/components/NavButton.vue'
@@ -33,16 +48,12 @@ export default {
   components: { InputField, NavButton },
   data() {
     return {
-      userData: {
-        userName: '',
-        eMailAddress: '',
-        passWord: '',
-        mobilityAssistance: '',
-        mobilityAssistanceWidth: ''
-      }
+      store: storeData()
     }
-    //methods: {
-    //editUserData() {
+  },
+  created() {
+    const dummyUser = this.store.userData[0]
+    console.log('userData:', dummyUser)
   }
 }
 </script>
