4bf096911c09d317c80d57a797e5c6c8da57bcf9
mullerow <maximilian.kohler@posteo.de>
2024-04-15 11:16:25 +0200

    chore: HTML code changed to the right View

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -23,40 +23,7 @@ export default {
     <li><RouterLink :to="{ name: 'searchpoi' }">Suche</RouterLink></li>
     <li><RouterLink :to="{ name: 'ownpoi' }">geteilte Orte</RouterLink></li>
   </ul>
-  <h3>Berechnungsbeispiel Koordinaten Distanz</h3>
-  <h4>Poi Koordinaten:</h4>
-  <p>X-Koordinaten: {{ store.poiData[0].xCoordinates }}</p>
-  <p>Y-Koordinaten: {{ store.poiData[0].yCoordinates }}</p>
 
-  <h3>Eigenen Standort ermitteln</h3>
-  <button @click="store.getOwnPosition">Eigener Standort</button>
-  <p>X-Koordinaten: {{ store.ownXCoordinate }}</p>
-  <p>Y-Koordinaten: {{ store.ownYCoordinate }}</p>
-
-  <button @click="store.calcDistance(store.poiData[0], store.ownXCoordinate, store.ownYCoordinate)">
-    Berechne Distanz
-  </button>
-  <h3>Differenzen der Koordinaten</h3>
-  <p>Ergebnis X: {{ store.xCoordinateDifference }}</p>
-  <p>Ergebnis Y: {{ store.yCoordinateDifference }}</p>
-  <h3>Differenzen der Längen in X und Y Richtung</h3>
-  <p>Distanz X: {{ store.xlengthDifference }} Meter</p>
-  <p>Distanz Y: {{ store.ylengthDifference }} Meter</p>
-  <h2>
-    Luftlinie zum Ziel:
-    <span style="color: goldenrod">{{ store.straightLineToAim }}</span>
-    Meter
-  </h2>
-  <button @click="store.getAddressbyCoordinates(store.ownXCoordinate, store.ownYCoordinate)">
-    ermittle die Addresse
-  </button>
-  <h3>Addresse des Ziels:</h3>
-  <p>Stadt: {{ store.city }}</p>
-  <p>Stadteil: {{ store.district }}</p>
-  <p>Straße: {{ store.street }}</p>
-  <p>Postleitzahl: {{ store.zipCode }}</p>
-  <p v-show="store.houseNumber">Hausnummer: {{ store.houseNumber }}</p>
-  <p v-show="!store.houseNumber">Hausnummer: nicht vorhanden (Daten nicht genau zuweisbar)</p>
   <!--
   <h3>Liste aller User aus der Pinia Datenbank</h3>
   <ul v-for="user of store.userData" :key="user.id">
