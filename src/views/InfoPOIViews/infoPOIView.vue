dd162d15ebe8e9817164dfd26e7f94b1682fb9a3
Eric <rumperic@gmail.com>
2024-04-16 10:51:17 +0200

    Search info und Info field componente angepasst (props eingefügt und interface erstellt)

--- src/views/InfoPOIViews/infoPOIView.vue
+++ src/views/InfoPOIViews/infoPOIView.vue
@@ -8,17 +8,26 @@
 
   <br />
   <h2>Info</h2>
-
+  <InfoField />
+  <LöschenButton :Löschen="'Nicht nutzbar'" />
+  <NavButton :Navigation="'In Maps öffnen'" />
   <RouterLink :to="{ name: 'infopoicomment' }">Comments</RouterLink>
 </template>
 
 <script>
 import HomeButton from '@/components/HomeButton.vue'
 import BackArrow from '@/components/BackArrow.vue'
+import InfoField from '@/components/InfoField.vue'
+import NavButton from '@/components/NavButton.vue'
+import LöschenButton from '@/components/LöschenButton.vue'
+
 export default {
   components: {
     HomeButton,
-    BackArrow
+    BackArrow,
+    InfoField,
+    NavButton,
+    LöschenButton
   }
 }
 </script>
