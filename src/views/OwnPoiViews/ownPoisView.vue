a1eca86efafd82c5cbaa5b0cc6da5e2e37fadaf9
Eric <rumperic@gmail.com>
2024-04-17 10:28:10 +0200

    feat buttons eingefügt in own poi resultlist

--- src/views/OwnPoiViews/ownPoisView.vue
+++ src/views/OwnPoiViews/ownPoisView.vue
@@ -8,13 +8,24 @@
   <RouterLink :to="{ name: 'ownpoimap' }">ICON Erde</RouterLink> -->
   <h2>Hier findest du alle Orte die du erstellt hast</h2>
 
+  <RouterLink :to="{ name: 'ownpoiinfo' }"
+    ><CategorieButton v-for="poi of store.poiData" :key="poi.id" :Kategorie="poi.poiName"
+  /></RouterLink>
+
   <RouterLink :to="{ name: 'ownpoiinfo' }">Bearbeiten</RouterLink>
 </template>
+
+<script setup>
+import { storeData } from '@/stores/store.js'
+
+const store = storeData()
+</script>
 
 <script>
 import HomeButton from '@/components/HomeButton.vue'
 import EarthMap from '@/components/EarthMap.vue'
+import CategorieButton from '@/components/CategorieButton.vue'
 export default {
-  components: { HomeButton, EarthMap }
+  components: { HomeButton, EarthMap, CategorieButton }
 }
 </script>
