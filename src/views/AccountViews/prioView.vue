497ce318c3102a575e586d8547255867c19548bf
Vanessa Pfohmann <v.pfohmann@googlemail.com>
2024-04-21 23:18:17 +0200

    feat: adds function to add User to API and function to get User id from API into the local storage for further useage in other views.vue

--- src/views/AccountViews/prioView.vue
+++ src/views/AccountViews/prioView.vue
@@ -19,18 +19,14 @@
   <div v-if="showPopup" class="popup">
     <div class="popup-content">
       <p>Dein Account wird angelegt.</p>
-      <RouterLink :to="{ name: 'home' }"
-        ><button @click="addUserAndNavigate">OK</button></RouterLink
-      >
+      <button @click="addUserAndNavigate">OK</button>
+      <RouterLink :to="{ name: 'home' }"></RouterLink>
     </div>
   </div>
 </template>
 
-<script setup>
-import { storeData } from '@/stores/store.js'
-</script>
-
 <script>
+import { storeData } from '@/stores/store.js'
 import PriorityButton from '@/components/PriorityButton.vue'
 import NavButton from '@/components/NavButton.vue'
 
@@ -53,6 +49,7 @@ export default {
     },
     saveDataAndOpenPopup() {
       this.saveDataToLocalStorage()
+      this.loadUserDataToTemporaryData()
       this.showPopup = true
     },
     saveDataToLocalStorage() {
@@ -62,45 +59,68 @@ export default {
       localUserData.mobilityAssistanceWidth = this.mobilityAssistanceWidth
 
       localStorage.setItem('userData', JSON.stringify(localUserData))
-    }
-  },
-  async addUserAndNavigate() {
-    //Schritt 1 user daten laden
-    this.loadUserDataFromLocalStorage()
-
-    //erfolgreich?. dann
-    if (!this.addingUser) {
-      this.addingUser = true
+    },
+    loadUserDataToTemporaryData() {
       try {
+        // Schritt 1: Benutzerdaten aus dem lokalen Speicher laden
         const localUserData = JSON.parse(localStorage.getItem('userData')) || {}
 
-        // Schritt 2 User an API
-        const response = await this.store.addNewUserToAPI(localUserData)
-        console.log('API-Antwort:', response)
-
-        // Schritt 3 User von API mit neuer ID
-        const userDataFromAPI = await this.store.getUserDataFromAPI()
-
-        if (response && response.id && userDataFromAPI) {
-          // Schritt4  ID und User in local Storage
-          this.saveUserIdToLocalStorage(response.id)
-          localStorage.setItem('userData', JSON.stringify(userDataFromAPI))
-
-          // Schritt 5 zur Landing Page
-          this.$router.push({ name: 'home' })
-        } else {
-          console.error('Fehler: Ungültige API-Antwort')
+        // Schritt 2: Benutzerdaten in temporäre Daten im Store setzen
+        this.store.temporaryData.newUserData = {
+          id: '',
+          username: localUserData.username || '',
+          email: localUserData.email || '',
+          mobilityAssistance: localUserData.mobilityAssistance || '',
+          mobilityAssistanceWidth: localUserData.mobilityAssistanceWidth || '',
+          password: localUserData.password || ''
         }
+
+        console.log(
+          'Benutzerdaten erfolgreich in temporäre Daten geladen:',
+          this.store.temporaryData.newUserData
+        )
+      } catch (error) {
+        console.error('Fehler beim Laden der Benutzerdaten in temporäre Daten:', error)
+      }
+    },
+
+    async addUserAndNavigate() {
+      try {
+        // Schritt 1: Benutzerdaten aus dem lokalen Speicher laden
+        const localUserData = JSON.parse(localStorage.getItem('userData')) || {}
+
+        // Schritt 2: Benutzerdaten in temporäre Daten im Store setzen
+        this.store.temporaryData.newUserData = localUserData
+
+        // Schritt 3: Benutzer an die API senden und auf Antwort warten
+        await this.store.addNewUserToAPI()
+
+        // Schritt 4: Benutzerdaten von der API abrufen
+        //const data =
+        await this.store.getUserDataFromAPI()
+
+        console.log('Daten von der API erhalten:', this.store.temporaryData.currentUser)
+
+        //speichern von der API im lokalen Speicher
+        //this.store.temporaryData.currentUser
+        //console.log('Aktuelle Benutzerdaten:', currentUser)
+        localStorage.setItem(
+          'currentUserID',
+          JSON.stringify(
+            this.store.temporaryData.currentUser[this.store.temporaryData.currentUser.length - 1].id
+          )
+        )
+
+        // Schritt 5: Weiterleitung zur Landing Page
+        this.$router.push({ name: 'home' })
       } catch (error) {
         console.error('Fehler beim Hinzufügen des Benutzers:', error)
-      } finally {
-        //ladezustand ENDE
-        this.addingUser = false
       }
     }
   }
 }
 </script>
+
 <style>
 .input {
   padding: 0.5rem;
