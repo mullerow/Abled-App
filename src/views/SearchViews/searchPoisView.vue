08dbbeba7ba012c9f3226c8fef46d7469737df1a
mullerow <maximilian.kohler@posteo.de>
2024-05-03 11:14:49 +0200

    searchpoiview is now responsive

--- src/views/SearchViews/searchPoisView.vue
+++ src/views/SearchViews/searchPoisView.vue
@@ -7,7 +7,7 @@
     </div>
   </header>
   <HeadLine Headline="Wo möchtest du suchen?" class="headline"></HeadLine>
-  <h3>SUCHRADIUS</h3>
+  <h3 class="search-radius-input-header">SUCHRADIUS</h3>
   <input
     type="range"
     min="50"
@@ -17,24 +17,28 @@
     name="rangeInput"
     id="idRangeInput"
   />
-  <label for="rangeInput">Reichweite {{ store.temporaryData.searchDistance }} m</label>
+  <label for="rangeInput" class="search-radius-label"
+    >Reichweite {{ store.temporaryData.searchDistance }} m</label
+  >
   <NavButton
     Navigation="Bestimme meinen Standort"
     @click="store.getOwnPosition"
     class="router-link"
   ></NavButton>
   <br />
-  <h3>Sie befinden sich hier:</h3>
-  <p><b>Stadt:</b> {{ store.temporaryData.city }}</p>
-  <p><b>Stadteil:</b> {{ store.temporaryData.district }}</p>
-  <p><b>Straße:</b> {{ store.temporaryData.street }}</p>
-  <p><b>Postleitzahl:</b> {{ store.temporaryData.zipCode }}</p>
-  <p v-show="store.temporaryData.houseNumber">
-    <b>Hausnummer:</b> {{ store.temporaryData.houseNumber }}
-  </p>
-  <p v-show="!store.temporaryData.houseNumber">
-    <b>Hausnummer:</b> nicht vorhanden (Daten nicht genau zuweisbar)
-  </p>
+  <div class="location-info-container">
+    <h3 class="current-location-info">Sie befinden sich hier:</h3>
+    <p class="current-location-info"><b>Stadt:</b> {{ store.temporaryData.city }}</p>
+    <p class="current-location-info"><b>Stadteil:</b> {{ store.temporaryData.district }}</p>
+    <p class="current-location-info"><b>Straße:</b> {{ store.temporaryData.street }}</p>
+    <p class="current-location-info"><b>Postleitzahl:</b> {{ store.temporaryData.zipCode }}</p>
+    <p v-show="store.temporaryData.houseNumber" class="current-location-info">
+      <b>Hausnummer:</b> {{ store.temporaryData.houseNumber }}
+    </p>
+    <p v-show="!store.temporaryData.houseNumber" class="current-location-info">
+      <b>Hausnummer:</b> nicht vorhanden (Daten nicht genau zuweisbar)
+    </p>
+  </div>
 
   <RouterLink :to="{ name: 'searchcategorie' }" class="router-link"
     ><NavButton Navigation="Weiter">Weiter</NavButton>
@@ -43,7 +47,6 @@
 
 <script>
 import { storeData } from '@/stores/store.js'
-// import HomeButton from '@/components/HomeButton.vue'
 import headerLogo from '@/components/headerLogo.vue'
 import BackArrow from '@/components/BackArrow.vue'
 import NavButton from '@/components/NavButton.vue'
@@ -51,7 +54,6 @@ import HeadLine from '@/components/HeadLine.vue'
 
 export default {
   components: {
-    // HomeButton,
     headerLogo,
     BackArrow,
     NavButton,
@@ -103,5 +105,25 @@ label {
 }
 .router-link {
   align-self: center;
+}
+#idRangeInput {
+  width: 40%;
+  margin-left: 30%;
+}
+.search-radius-label {
+  margin-left: 30%;
+}
+.search-radius-input-header {
+  margin-left: 30%;
+}
+.current-location-info {
+  margin-left: 15px;
+}
+.location-info-container {
+  margin-left: auto;
+  margin-right: auto;
+  background-color: var(--white);
+  color: var(--black);
+  border-radius: 16px;
 }
 </style>
