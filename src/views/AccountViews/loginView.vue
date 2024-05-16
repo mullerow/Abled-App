22d6f9afc5dc5d99f3b8e85d95579e7f1403e313
Vanessa Pfohmann <v.pfohmann@googlemail.com>
2024-05-16 18:03:22 +0200

    feat: adds method to check if username exists on api

--- src/views/AccountViews/loginView.vue
+++ src/views/AccountViews/loginView.vue
@@ -1,8 +1,12 @@
 <template>
   <h1 class="heading-login">Willkommen zurück</h1>
-  <InputField class="input-login" :value="username" placeholder="Benutzername eingeben" label>
-    ></InputField
-  >
+  <InputField
+    class="input-login"
+    :value="username"
+    @input="onUsernameInput"
+    placeholder="Benutzername eingeben"
+    label
+  ></InputField>
 
   <InputField class="input-login" :value="password" type="password" placeholder="Passwort eingeben">
     ></InputField
@@ -22,7 +26,59 @@ export default {
 
   data() {
     return {
-      store: storeData()
+      store: storeData(),
+      username: '',
+      password: '',
+      timeoutId: null
+    }
+  },
+
+  created() {
+    this.fetchUserDataFromAPI()
+  },
+  methods: {
+    async fetchUserDataFromAPI() {
+      try {
+        await this.store.getUserDataFromAPI()
+        const userData = this.store.temporaryData.currentUserData
+        console.log('Benutzerdaten von der API:', userData)
+      } catch (error) {
+        console.error('Fehler beim Abrufen der Benutzerdaten:', error)
+      }
+    },
+    async onUsernameInput(event) {
+      this.username = event.target.value
+      if (this.timeoutId !== null) {
+        clearTimeout(this.timeoutId)
+      }
+
+      this.timeoutId = setTimeout(() => {
+        this.checkUsername()
+      }, 5000)
+    },
+
+    async checkUsername() {
+      console.log(this.username)
+      if (!this.username || this.username.trim() === '') {
+        console.log('Ungültiger Benutzername')
+        return
+      }
+
+      try {
+        await this.fetchUserDataFromAPI()
+
+        const userData = this.store.temporaryData.currentUserData
+
+        const userExists = userData.some((user) => user.username === this.username)
+        console.log(typeof this.username)
+        if (userExists) {
+          console.log('Benutzer vorhanden')
+        } else {
+          console.log('Benutzer nicht gefunden')
+        }
+      } catch (error) {
+        console.error('Fehler beim Abrufen der Benutzerdaten:', error)
+      }
     }
   }
 }
