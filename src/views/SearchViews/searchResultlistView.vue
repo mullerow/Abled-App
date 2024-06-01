6958282e80f6ad71a1cf93f428f7eb84b468401b
mullerow <maximilian.kohler@posteo.de>
2024-06-01 10:29:14 +0200

    chore: infopopup added to resultlistView

--- src/views/SearchViews/searchResultlistView.vue
+++ src/views/SearchViews/searchResultlistView.vue
@@ -1,4 +1,48 @@
 <template>
+  <!--  INFO POPUP für den standort bestimmungsbutton -->
+  <div class="resultlist-popup-container" v-if="resultlistPopup">
+    <svg
+      class="resultlist-svg-popup"
+      viewBox="0 0 100 100"
+      width="200px"
+      height="200px"
+      style="filter: url(#f1)"
+    >
+      <defs>
+        <filter id="f1" x="-10%" y="-10%" width="140%" height="140%">
+          <feGaussianBlur in="SourceAlpha" stdDeviation="0.5" result="blur" />
+          <feColorMatrix
+            in="blur"
+            type="matrix"
+            values="0 0 0 0 0.5 0 0 0 0 0.5 0 0 0 0 0.5 0 0 0 1 0"
+            result="coloredBlur"
+          />
+          <feOffset in="coloredBlur" dx="3" dy="3" result="offsetBlur" />
+          <feMerge>
+            <feMergeNode in="offsetBlur" />
+            <feMergeNode in="SourceGraphic" />
+          </feMerge>
+        </filter>
+        <clipPath id="clipPolygon">
+          <path
+            d="
+            M 82.004 38.891 C 91.22 34.704 91.451 23.504 85.741 14.014 C 82.204 6.959 78.02 8.645 72.502 14.77 L 75.513 14.77 L 70.963 19.321 L 67.081 14.904 L 69.825 14.77 C 71.341 12.106 73.071 10.66 77.691 7.499 C 74.058 7.935 73.288 7.678 71.066 7.123 L 69.869 8.96 L 67.433 3.661 L 73.117 3.404 L 72.092 5.199 C 80.583 6.302 85.822 4.371 90.724 9.675 C 95.418 15.967 98.195 16.76 98.459 28.265 V 85.669 C 98.964 92.982 98.935 92.982 95.977 92.953 C 92.033 92.982 96.023 100.001 91.979 99.921 H 7.04 C 3.167 99.949 6.953 93.025 2.972 92.967 C 0.072 93.025 0.043 92.996 0.429 84.458 V 42.999 C 0.567 39.019 0.567 38.927 5.287 39.019 Z"
+          />
+        </clipPath>
+      </defs>
+      <rect width="200px" height="200px" clip-path="url(#clipPolygon)" style="fill: var(--black)" />
+    </svg>
+    <popupButtonTutorial
+      class="resultlist-popup-button"
+      :buttonLabel="'Geht klar!'"
+      @click="searchPoiPopup = false"
+    />
+    <p class="resultlist-popup-text">
+      Lege fest, in welchem Radius du Orte auf der Karte angezeigt bekommen möchtest und bitte
+      stimme der Standortbestimmung zu, damit wir Dir dann auch interessante Orte in deiner Nähe
+      zeigen können.
+    </p>
+  </div>
   <div class="header-buttons">
     <RouterLink :to="{ name: 'searchoptionalcategorie' }"> <BackArrow /></RouterLink>
     <RouterLink :to="{ name: 'home' }"> <headerLogo /></RouterLink>
@@ -34,17 +78,20 @@ import EarthMap from '@/components/EarthMap.vue'
 import BackArrow from '@/components/BackArrow.vue'
 import headerLogo from '@/components/headerLogo.vue'
 import HeadLine from '@/components/HeadLine.vue'
+import popupButtonTutorial from '@/components/popupButtonTutorial.vue'
 
 export default {
   components: {
     headerLogo,
     EarthMap,
     BackArrow,
-    HeadLine
+    HeadLine,
+    popupButtonTutorial
   },
   data() {
     return {
-      store: storeData()
+      store: storeData(),
+      resultlistPopup: true
     }
   },
   created() {
@@ -109,6 +156,31 @@ b {
   top: 10px;
   right: 10px;
   width: 30px;
+  border-radius: 5px;
+}
+.resultlist-popup-container {
+  position: relative;
+  align-self: center;
+  width: 300px;
+  z-index: 10;
+}
+.resultlist-svg-popup {
+  position: absolute;
+  width: 300px;
+  height: 300px;
+  top: 195px;
+  left: 20px;
+}
+.resultlist-popup-button {
+  position: absolute;
+  top: 415px;
+  left: 173px;
+}
+.resultlist-popup-text {
+  position: absolute;
+  top: 305px;
+  left: 50px;
+  color: var(--white);
 }
 
 @media screen and (min-width: 600px) {
