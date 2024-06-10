b382658f9750d25c1f412cce77957c7565294239
mullerow <maximilian.kohler@posteo.de>
2024-06-10 12:55:08 +0200

    chore: add a list of favorite poi buttons to the favoritepoiView

--- src/views/FavouritesViews/favoritePoiView.vue
+++ src/views/FavouritesViews/favoritePoiView.vue
@@ -6,16 +6,51 @@
 
   <h2>Hier findest du alle deine favorisierten Orte</h2>
 
+  <div
+    class="favorite-poi-container"
+    v-for="favoritePoi of this.store.temporaryData.DataFromCurrentUser.favoritePoisOfUser"
+    :key="favoritePoi"
+  >
+    <button class="favorite-poi-button">{{ favoritePoi }}</button>
+  </div>
+
   <RouterLink :to="{ name: 'favoritepoiinfo' }">Weiter</RouterLink>
 </template>
 
 <script>
 import HomeButton from '@/components/HomeButton.vue'
 import EarthMap from '@/components/EarthMap.vue'
+import { storeData } from '@/stores/store.js'
+
 export default {
   components: {
     HomeButton,
     EarthMap
+  },
+  data() {
+    return {
+      store: storeData()
+    }
+  },
+  created() {
+    this.store.getUserDataFromAPI().then(() => {
+      this.store.getDataFromCurrentUser()
+      console.log(
+        'this.store.temporaryData.currentUserData',
+        this.store.temporaryData.currentUserData
+      )
+    })
   }
 }
 </script>
+<style scoped>
+.favorite-poi-container {
+  margin-left: auto;
+  margin-right: auto;
+}
+.favorite-poi-button {
+  width: 200px;
+  height: 50px;
+  margin-bottom: 10px;
+}
+</style>
