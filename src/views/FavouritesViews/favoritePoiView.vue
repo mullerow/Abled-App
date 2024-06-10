557917f2c603e0b8a88fedd69439dad0685e4998
mullerow <maximilian.kohler@posteo.de>
2024-06-10 16:26:23 +0200

    chore: the list of favorite pois is now displayed at the favoritepoiView

--- src/views/FavouritesViews/favoritePoiView.vue
+++ src/views/FavouritesViews/favoritePoiView.vue
@@ -1,45 +1,62 @@
 <template>
+  <BackgroundGradient />
   <div class="header-buttons">
-    <RouterLink :to="{ name: 'ownpoimap' }"><EarthMap /></RouterLink>
-    <RouterLink :to="{ name: 'home' }"> <HomeButton /></RouterLink>
+    <RouterLink :to="{ name: 'home' }"> <BackArrow /></RouterLink>
+    <RouterLink :to="{ name: 'home' }"> <headerLogo /></RouterLink>
+    <RouterLink :to="{ name: '' }"><EarthMap /></RouterLink>
   </div>
-
-  <h2>Hier findest du alle deine favorisierten Orte</h2>
+  <HeadLine
+    Headline="Hier findest du alle deine favorisierten Orte"
+    class="headline-text"
+  ></HeadLine>
 
   <div
     class="favorite-poi-container"
-    v-for="favoritePoi of this.store.temporaryData.DataFromCurrentUser.favoritePoisOfUser"
+    v-for="favoritePoi of this.store.temporaryData.dataFromFavoritePois"
     :key="favoritePoi"
   >
-    <button class="favorite-poi-button">{{ favoritePoi }}</button>
+    <button class="favorite-poi-button">
+      {{ favoritePoi.poiName }}
+      <img
+        class="categorie-icon"
+        :src="this.store.getIconOfCategory(favoritePoi)"
+        alt="Kategorie Icon"
+      />
+    </button>
   </div>
-
-  <RouterLink :to="{ name: 'favoritepoiinfo' }">Weiter</RouterLink>
 </template>
 
 <script>
-import HomeButton from '@/components/HomeButton.vue'
 import EarthMap from '@/components/EarthMap.vue'
+import BackArrow from '@/components/BackArrow.vue'
+import headerLogo from '@/components/headerLogo.vue'
 import { storeData } from '@/stores/store.js'
+import BackgroundGradient from '@/components/BackgroundGradient.vue'
+import HeadLine from '@/components/HeadLine.vue'
 
 export default {
   components: {
-    HomeButton,
-    EarthMap
+    BackArrow,
+    headerLogo,
+    EarthMap,
+    BackgroundGradient,
+    HeadLine
   },
   data() {
     return {
       store: storeData()
     }
   },
-  created() {
-    this.store.getUserDataFromAPI().then(() => {
-      this.store.getDataFromCurrentUser()
-      console.log(
-        'this.store.temporaryData.currentUserData',
-        this.store.temporaryData.currentUserData
-      )
-    })
+  async created() {
+    await this.store.getPoiDataFromAPI()
+    await this.store.getUserDataFromAPI()
+    this.store.getDataFromCurrentUser()
+    console.log(
+      'this.store.temporaryData.currentUserData',
+      this.store.temporaryData.currentUserData
+    )
+
+    this.store.getDataFromFavoritePois()
   }
 }
 </script>
@@ -47,10 +64,18 @@ export default {
 .favorite-poi-container {
   margin-left: auto;
   margin-right: auto;
+  position: relative;
 }
 .favorite-poi-button {
   width: 200px;
   height: 50px;
   margin-bottom: 10px;
+}
+.categorie-icon {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  width: 30px;
+  border-radius: 5px;
 }
 </style>
