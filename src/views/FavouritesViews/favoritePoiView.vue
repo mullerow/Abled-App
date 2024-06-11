058ab4cbfeba5992dcbf6bf5c5e44c7f364e85f6
mullerow <maximilian.kohler@posteo.de>
2024-06-11 13:22:53 +0200

    chore: the detailcategories are now shown at the favoritepoiView

--- src/views/FavouritesViews/favoritePoiView.vue
+++ src/views/FavouritesViews/favoritePoiView.vue
@@ -15,13 +15,24 @@
     v-for="favoritePoi of this.store.temporaryData.dataFromFavoritePois"
     :key="favoritePoi"
   >
-    <button class="favorite-poi-button">
-      {{ favoritePoi.poiName }}
-      <img
-        class="categorie-icon"
-        :src="this.store.getIconOfCategory(favoritePoi)"
-        alt="Kategorie Icon"
-      />
+    <button class="favoritelist-button">
+      <router-link :to="'/infopoi/' + favoritePoi.id">
+        <div class="favoritelist-poiname">{{ favoritePoi.poiName }}</div>
+        <img
+          class="categorie-icon"
+          :src="this.store.getIconOfCategory(favoritePoi)"
+          alt="Kategorie Icon"
+        />
+        <div class="detailcategories-container">
+          <div
+            class="favoritelist-detailcategories"
+            v-for="detailcategorie of favoritePoi.detailCategories"
+            :key="'detail-' + favoritePoi.id + '-' + detailcategorie"
+          >
+            ✅ {{ detailcategorie }}
+          </div>
+        </div>
+      </router-link>
     </button>
   </div>
 </template>
@@ -65,17 +76,62 @@ export default {
   margin-left: auto;
   margin-right: auto;
   position: relative;
+  width: 100%;
 }
-.favorite-poi-button {
-  width: 200px;
-  height: 50px;
-  margin-bottom: 10px;
-}
+
 .categorie-icon {
   position: absolute;
   top: 10px;
   right: 10px;
   width: 30px;
   border-radius: 5px;
+}
+.favoritelist-button {
+  position: relative;
+  width: 100%;
+  min-height: 100px;
+  background-color: var(--white);
+  color: var(--red);
+  margin-bottom: 10px;
+  border-radius: 16px;
+  border: 1px solid var(--black);
+  align-self: center;
+  justify-content: space-around;
+}
+.favoritelist-button:hover {
+  background-color: rgb(252, 220, 180);
+  cursor: pointer;
+}
+.favoritelist-detailcategories {
+  text-align: start;
+  font-size: 14px;
+  width: 70%;
+}
+.detailcategories-container {
+  display: flex;
+  flex-direction: column;
+  min-height: 50px;
+  width: 300px;
+  justify-content: flex-start;
+}
+
+.favoritelist-poiname {
+  font-size: 20px;
+  font-weight: bold;
+  color: var(--red);
+  align-self: flex-start;
+}
+.headline-text {
+  margin-bottom: 30px;
+}
+@media screen and (min-width: 600px) {
+  .favoritelist-button {
+    width: 70%;
+  }
+}
+@media screen and (min-width: 768px) {
+  .favoritelist-button {
+    width: 50%;
+  }
 }
 </style>
